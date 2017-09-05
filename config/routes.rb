3cddd9913f1c5fcb38a25109c85871cde2c7dfc2
Celia Mutschler <celia.mutschler@web.de>
2017-09-05 11:52:31 +0200

    exercise done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get "/tasks", to: "tasks#"
-  # get "/tasks/:id", to: "tasks#"
-  # get "/tasks/new", to: "tasks#"
-  # post "/tasks", to: "tasks#"
-  # get "/tasks/:id/edit", to: "tasks#"
-  # patch "/tasks/:id", to: "tasks#"
-  # delete "/tasks/:id", to: "tasks#"
+  root 'tasks#index'
+  resources :tasks
+  patch 'tasks/:id/complete', to: 'tasks#complete', as: :complete
 end
--- db/migrate/20170905073442_create_tasks.rb
+++ db/migrate/20170905073442_create_tasks.rb
@@ -1,8 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.integer :id
+      t.string :title
+      t.string :description
+      t.boolean :completed, default: false
+      t.date :deadline, default: nil
 
       t.timestamps
     end
